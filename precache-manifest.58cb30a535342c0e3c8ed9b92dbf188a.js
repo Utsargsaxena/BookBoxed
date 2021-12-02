78b5d50e0b3d1605bb7e8b35aca74be725adc275
Utsarg Saxena <45102149+Utsargsaxena@users.noreply.github.com>
2021-12-02 15:47:03 +0530

    Updates

---
+++ precache-manifest.58cb30a535342c0e3c8ed9b92dbf188a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a916332b08d2fec0dc29e5917ebca79",
+    "url": "/BookBoxed/index.html"
+  },
+  {
+    "revision": "27fb2cf6a1dec7221851",
+    "url": "/BookBoxed/static/css/main.cfeb5ae3.chunk.css"
+  },
+  {
+    "revision": "dacefdba79ccf8703acc",
+    "url": "/BookBoxed/static/js/2.d15eae47.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/BookBoxed/static/js/2.d15eae47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fb2cf6a1dec7221851",
+    "url": "/BookBoxed/static/js/main.fb7ce4a6.chunk.js"
+  },
+  {
+    "revision": "15234df8ae78892cdd42",
+    "url": "/BookBoxed/static/js/runtime-main.994e01b1.js"
+  },
+  {
+    "revision": "348d66fdc31f6639354ab9785d05af38",
+    "url": "/BookBoxed/static/media/books.348d66fd.svg"
+  }
+]);
